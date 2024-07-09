17eb475f802eebcc46c33ad54073bc396115b37b
vincentdnl <denielvincent@gmail.com>
2024-07-10 00:51:45 +0200

    copying file to tiktok folder

--- main.py
+++ main.py
@@ -1,8 +1,11 @@
 import os
+import shutil
 from pathlib import Path
 
 from PIL import Image
 import argparse
+
+# C:\Users\vince\src\comicut\venv\Scripts\python.exe main.py C:\Users\vince\iCloudDrive\dessins\
 
 
 def slice_image(source_image_path, number_cols_rows, margin):
@@ -13,6 +16,8 @@ def slice_image(source_image_path, number_cols_rows, margin):
     width = i.width
     height = i.height
     pieces = 0
+
+    filenames = []
     for y in range(number_cols_rows):
         for x in range(number_cols_rows):
             index = pieces
@@ -24,7 +29,25 @@ def slice_image(source_image_path, number_cols_rows, margin):
 
             result.paste(img, (x * int(margin / 2), y * int(margin / 2)))
 
+            filenames.append(f"{image_name}_p{index + 1}{image_suffix}")
             result.save(Path(f"{base_path}/{image_name}_p{index + 1}{image_suffix}"))
+    return filenames
+
+
+def copy_files(file_names, base_path, destination_path):
+    # Ensure the destination directory exists
+    os.makedirs(destination_path, exist_ok=True)
+
+    for file_name in file_names:
+        # Construct the full file path
+        src_file = os.path.join(base_path, file_name)
+        dest_file = os.path.join(destination_path, file_name)
+
+        try:
+            shutil.copy(src_file, dest_file)
+            print(f"Copied {file_name} to {destination_path}")
+        except FileNotFoundError:
+            print(f"File {file_name} not found in {base_path}")
 
 
 if __name__ == '__main__':
@@ -32,4 +55,10 @@ if __name__ == '__main__':
     parser.add_argument("source_image_path", help="The source image that will be split", type=str)
     args = parser.parse_args()
 
-    slice_image(Path(args.source_image_path), 2, 60)
+    print("Splitting image...")
+    filenames = slice_image(Path(args.source_image_path), 2, 60)
+    print("Finished splitting image.")
+
+    print("Copying files to TikTok folder")
+    copy_files(filenames, os.path.dirname(Path(args.source_image_path)), Path("G:\\My Drive\\for tiktok"))
+    print("Files copied.")
